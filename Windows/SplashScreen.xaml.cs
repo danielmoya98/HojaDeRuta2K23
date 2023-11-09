1a4d80b77483f6af803c80956ab348f1e0ff7cb9
Alienware <moycarlos09@gmail.com>
2023-11-08 23:41:17 -0400

    v6

---
+++ Windows/SplashScreen.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace HojadeRuta2K23.Windows;
+
+public partial class SplashScreen : Window
+{
+    public SplashScreen()
+    {
+        InitializeComponent();
+        
+    }
+}

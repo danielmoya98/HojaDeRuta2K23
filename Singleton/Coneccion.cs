e1321938a3ca5545d4e26d9b10ef4f300f8c7401
Alienware <moycarlos09@gmail.com>
2023-11-20 15:54:42 -0400

    v9

---
+++ Singleton/Coneccion.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HojadeRuta2K23.Singleton;
+
+public class Coneccion
+{
+    private static Coneccion instacia;
+    private readonly string cadenaDeConeccion;
+
+    private Coneccion()
+    {
+        cadenaDeConeccion =
+            
+            "Server=Italo-Navia;Database=nashe1;Integrated Security=true; TrustServerCertificate=True"; // Reemplaza con tu cadena de conexión
+   
+    }
+
+    public static Coneccion Instance
+    {
+        get
+        {
+            if (instacia == null)
+            {
+                instacia = new Coneccion();
+            }
+
+            return instacia;
+        }
+    }
+
+    public SqlConnection GetConnection()
+    {
+        return new SqlConnection(cadenaDeConeccion);
+    }
+}
